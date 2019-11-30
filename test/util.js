70fa46623ba93dcab49eba36481542eb83fa0961
magicdawn <magicdawn@qq.com>
2019-12-01 03:31:36 +0800

    feat(djradio): add support to djradio

---
+++ test/util.js
@@ -0,0 +1,14 @@
+const {normalizeUrl, getId} = require('../lib/util.js')
+
+describe('util', function() {
+  it('normalizeUrl', () => {
+    let url = 'http://music.163.com/#/playlist?id=12583200'
+    url = normalizeUrl(url)
+    url.should.equal('http://music.163.com/playlist?id=12583200')
+  })
+
+  it('.getId', () => {
+    const id = getId('https://music.163.com/#/program?id=2064329279')
+    id.should.equal('2064329279')
+  })
+})

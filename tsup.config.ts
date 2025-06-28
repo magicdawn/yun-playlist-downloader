fdae527e1e8b285b03971afee5dc1ecf124f62a7
magicdawn <magicdawn@qq.com>
2025-06-28 10:07:07 +0800

    chore: adjust tsconfig target

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/cli.ts', 'src/index.ts'],
   format: 'esm',
-  target: 'node16',
+  target: 'node20',
   clean: true,
   shims: true,
   esbuildOptions(options, context) {
